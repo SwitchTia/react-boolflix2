1dbcaa050ca24f5b0cc33d10bc6b490050580134
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-09 21:39:30 +0100

    Feat: moving the logic from App.jsx to Header.jsx wip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,60 @@
 import "./Header.css";
 
-function Header({ searchQuery, setSearchQuery, handleSearch }) {
+import { useState } from "react";
+import axios from "axios";
+
+
+
+function Header({ setMoviesList }) {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const BASE_URL = "https://api.themoviedb.org/3";
+
+    function handleSearch() {
+        if (!searchQuery) return;
+
+        axios.get(`${BASE_URL}/search/movie`, {
+            params: {
+                api_key: API_KEY,
+                query: searchQuery,
+                language: "it-IT"
+            }
+        })
+            .then((moviesResponse) => {
+                const movies = moviesResponse.data.results.map(movie => ({
+                    ...movie,
+                    type: "movie"
+                }));
+
+                return axios.get(`${BASE_URL}/search/tv`, {
+                    params: {
+                        api_key: API_KEY,
+                        query: searchQuery,
+                        language: "it-IT"
+                    }
+                })
+                    .then((tvResponse) => {
+                        const tvShows = tvResponse.data.results.map(show => ({
+                            ...show,
+                            type: "tv"
+                        }));
+
+                        setMoviesList([...movies, ...tvShows]);
+                    });
+            })
+            .catch((error) => {
+                console.error("Error searching:", error);
+            });
+    }
 
     function handleKeyPress(event) {
         if (event.key === "Enter") {
             handleSearch();
         }
     }
-///nkq,jGILQU W3AIOEwl-aku .4,gzjhrem,
+
     return (
         <header className="header">
             <div className="header-content">
